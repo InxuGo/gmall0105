c636d0df0fbeb90a227347015b970be1cfe3526e
inxu <inxu0317@163.com>
2019-12-02 00:04:32 +0800

    测试提交

--- demo/src/main/java/com/gmall/demo/DemoTest.java
+++ demo/src/main/java/com/gmall/demo/DemoTest.java
@@ -8,6 +8,9 @@ package com.gmall.demo;
  */
 public class DemoTest {
     public static void main(String[] args){
+        if(true){
+            System.out.println("333");
+        }
         System.out.println("112");
     }
 
