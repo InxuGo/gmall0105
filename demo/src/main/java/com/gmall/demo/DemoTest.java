7478f21e265dcaa667544591e681381bcbf1d75b
inxu <inxu0317@163.com>
2019-12-02 00:12:00 +0800

    测试提交

--- demo/src/main/java/com/gmall/demo/DemoTest.java
+++ demo/src/main/java/com/gmall/demo/DemoTest.java
@@ -10,6 +10,8 @@ public class DemoTest {
     public static void main(String[] args){
         if(true){
             System.out.println("333");
+        }else{
+            System.out.println("555");
         }
         System.out.println("112");
     }
