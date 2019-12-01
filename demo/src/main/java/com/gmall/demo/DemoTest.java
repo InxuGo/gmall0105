3be1d84b905f201418da7f8ec6e82f220027ae36
inxu <inxu0317@163.com>
2019-12-02 00:02:19 +0800

    测试提交

---
+++ demo/src/main/java/com/gmall/demo/DemoTest.java
@@ -0,0 +1,14 @@
+package com.gmall.demo;
+
+/**
+ * @program: gmall0105
+ * @Date: 2019/12/1 23:37
+ * @Author: Inxu
+ * @Description:
+ */
+public class DemoTest {
+    public static void main(String[] args){
+        System.out.println("112");
+    }
+
+}
